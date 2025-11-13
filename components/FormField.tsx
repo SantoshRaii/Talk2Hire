8a17cd71e543137162be92b2075a61720a25737a
SantoshRaii <Santoshrai_ec20b13_58@dtu.ac.in>
2025-11-13 19:54:11 +0530

    initial commit

---
+++ components/FormField.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Control, Controller, FieldValues, Path } from 'react-hook-form'
+import { Input } from './ui/input'
+
+interface FormFieldProps<T extends FieldValues>{
+   control: Control<T>;
+   name: Path<T>;
+   label: string;
+   placeholder?: string;
+   type?: 'text' | 'email' | 'password' | 'file'
+
+}
+
+const FormFields = <T extends FieldValues> ({control, name, label, placeholder, type = "text"}: FormFieldProps<T>) => (
+
+        <FormField name={name} control = {control} render={({field}) =>(
+            <FormItem>
+              <FormLabel className='label'>{label}</FormLabel>
+              <FormControl>
+                <Input className="input" placeholder={placeholder}  type={type} {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+        )}
+    />
+);
+
+export default FormFields
